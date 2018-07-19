136b75390c58cee0496721c433e4fe6981c072e3
Paul Adams <30651735+PSAIV@users.noreply.github.com>
2018-07-18 22:32:37 -0500

    Update and rename Problem_3_1.java to Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -1,6 +1,7 @@
+//Prompts the user for input and then computes a quadratic root(s) from those numbers, if one exists
 import java.util.*;
-//3.1
-public class Problem_3_1 {
+
+public class Quadratic {
 	
 	public static void main(String[] args){
 		//Allocate variables
@@ -17,7 +18,7 @@ public class Problem_3_1 {
 		//Calculate the discriminant
 		disc = Math.pow(b, 2.0) - 4 * a * c;
 		
-		//Check the and evaluate accordingly
+		//Check the disc and evaluate accordingly
 		if(disc > 0){
 			root1 = (-b + Math.sqrt(Math.pow(b, 2.0) - (4 * a * c))) / ( 2 * a);
 			root2 = (-b - Math.sqrt(Math.pow(b,  2.0) - (4 * a * c))) / (2 * a);
